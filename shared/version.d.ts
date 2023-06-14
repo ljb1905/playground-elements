1520e0f444d4cde336d739891054db354bc212ca
Jongbin Lee <jongbin.lee@kt.com>
2023-06-14 14:49:25 +0900

    test

--- shared/version.d.ts
+++ shared/version.d.ts
@@ -3,6 +3,6 @@
  * Copyright 2021 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-export declare const npmVersion = "0.17.0";
+export declare const npmVersion = "0.0.4";
 export declare const serviceWorkerHash = "1dae6563";
 //# sourceMappingURL=version.d.ts.map
